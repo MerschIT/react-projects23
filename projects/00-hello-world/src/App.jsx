96a4e25e1b112ea0fd32c2dc80cfd42a563d52ee
MerschIT <92496497+MerschIT@users.noreply.github.com>
2023-01-30 23:00:20 -0300

    twitter card name and image

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -3,6 +3,10 @@ import TwitterFollowCard from "./twitter-follow-card"
 
 export default function App (){
     return(
-        <TwitterFollowCard />
+        <>
+        <TwitterFollowCard name='Miguel Angel Duran' userName="midudev" />
+        <TwitterFollowCard name='Lorem ipsum' userName="lorem" />
+        <TwitterFollowCard name='Argentina' userName="argentina" />
+        </>
     )
 }
--- projects/00-hello-world/src/twitter-follow-card.jsx
+++ projects/00-hello-world/src/twitter-follow-card.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 
-export default function TwitterFollowCard(){
+
+export default function TwitterFollowCard({name, userName}){
+    
     return(
         <article className="tw-followCard">
             <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src="https://unavatar.io/midudev" alt="Avatar de Midudev" />
-                <div className="tw-followCard-info"><strong>Miguel Durán</strong>
-                <span className="tw-followCard-infoUserName">@midudev</span></div>
+                <img className="tw-followCard-avatar" 
+                src={`https://unavatar.io/${userName}`}
+                alt="Avatar de Midudev" />
+                <div className="tw-followCard-info"><strong>{name}</strong>
+                <span className="tw-followCard-infoUserName">@{userName}</span></div>
             </header>
             <aside>
                 <button className="tw-followCard-button">Seguir</button>
